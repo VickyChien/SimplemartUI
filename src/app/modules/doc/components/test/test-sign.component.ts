c46a8921ae09fc328864c0d87e0ddebcdc919fed
unknown <vicky.chien@bankpro.com.tw>
2019-01-10 16:04:38 +0800

    UI

---
+++ src/app/modules/doc/components/test/test-sign.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-sign',
+  templateUrl: './test-sign.component.html'
+})
+export class TestSignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/doc/doc.module.ts
+++ src/app/modules/doc/doc.module.ts
@@ -4,19 +4,22 @@ import { CommonModule } from '@angular/common';
 
 import { OrderAccDateComponent } from './components/order-acc-date/order-acc-date.component';
 import { TestComponent } from './components/test/test.component';
+import { TestSignComponent } from './components/test/test-sign.component';
 import { SharedModule } from "../../shared/shared.module"
 
 @NgModule({
   declarations: [
     OrderAccDateComponent,
-    TestComponent
+    TestComponent,
+    TestSignComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule.forChild([
       { path: 'OrderAccDateDetail', component: OrderAccDateComponent },
-      { path: 'Test', component: TestComponent }
+      { path: 'Test', component: TestComponent },
+      { path: 'TestSign', component: TestSignComponent }
     ])
   ]
 })
