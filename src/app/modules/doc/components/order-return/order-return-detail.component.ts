e7bad1fba72134963de307c56c446e4f6a6e5c91
unknown <vicky.chien@bankpro.com.tw>
2019-01-31 17:05:46 +0800

    退貨UI

---
+++ src/app/modules/doc/components/order-return/order-return-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'order-return-detail',
+  templateUrl: './order-return-detail.component.html'
+})
+export class OrderReturnDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/doc/doc.module.ts
+++ src/app/modules/doc/doc.module.ts
@@ -7,6 +7,7 @@ import { OrderAccDateApplyComponent } from './components/order-acc-date/order-ac
 import { OrderAccDateQueryComponent } from './components/order-acc-date/order-acc-date-query.component';
 import { OrderDetailComponent } from './components/order/order-detail.component';
 import { OrderQueryComponent } from './components/order/order-query.component';
+import { OrderReturnDetailComponent } from './components/order-return/order-return-detail.component';
 import { TestComponent } from './components/test/test.component';
 import { TestSignComponent } from './components/test/test-sign.component';
 import { TestViewSupplierComponent } from './components/test/test-view-supplier.component';
@@ -19,6 +20,7 @@ import { SharedModule } from "../../shared/shared.module"
     OrderAccDateQueryComponent,
     OrderDetailComponent,
     OrderQueryComponent,
+    OrderReturnDetailComponent,
     TestComponent,
     TestSignComponent,
     TestViewSupplierComponent
@@ -32,6 +34,7 @@ import { SharedModule } from "../../shared/shared.module"
       { path: 'OrderAccDateQuery', component: OrderAccDateQueryComponent },
       { path: 'OrderDetail', component: OrderDetailComponent },
       { path: 'OrderQuery', component: OrderQueryComponent },
+      { path: 'OrderReturnDetail', component: OrderReturnDetailComponent },
       { path: 'Test', component: TestComponent },
       { path: 'TestSign', component: TestSignComponent },
       { path: 'TestViewSupplier', component: TestViewSupplierComponent }
