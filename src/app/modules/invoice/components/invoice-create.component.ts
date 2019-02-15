553d23591b39fbe3413471cb86af96f14fc3256a
unknown <vicky.chien@bankpro.com.tw>
2019-02-15 16:44:53 +0800

    發票開立UI

---
+++ src/app/modules/invoice/components/invoice-create.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'invoice-create',
+  templateUrl: './invoice-create.component.html'
+})
+export class InvoiceCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/invoice/invoice.module.ts
+++ src/app/modules/invoice/invoice.module.ts
@@ -3,18 +3,21 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 import { InvoiceQueryComponent } from './components/invoice-query.component';
+import { InvoiceCreateComponent } from './components/invoice-create.component';
 
 import { SharedModule } from "../../shared/shared.module"
 
 @NgModule({
   declarations: [
-    InvoiceQueryComponent
+    InvoiceQueryComponent,
+    InvoiceCreateComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule.forChild([
       { path: 'InvoiceQuery', component: InvoiceQueryComponent },
+      { path: 'InvoiceCreate', component: InvoiceCreateComponent },
     ])
   ]
 })
