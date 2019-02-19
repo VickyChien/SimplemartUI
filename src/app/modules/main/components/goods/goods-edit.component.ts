231be90cf4d59ee11c2ac8eb18776fe570f4ea3a
VickyChien <maomo1978@gmail.com>
2019-02-19 22:54:20 +0800

    商品主檔UI

---
+++ src/app/modules/main/components/goods/goods-edit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'goods-edit',
+  templateUrl: './goods-edit.component.html'
+})
+export class GoodsEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
