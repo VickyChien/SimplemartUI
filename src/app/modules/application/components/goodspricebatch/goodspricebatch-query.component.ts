7f37184e10422f44a1be3748fb95aa01c3d1c0c1
unknown <vicky.chien@bankpro.com.tw>
2019-04-03 16:55:45 +0800

    批次商品變價

---
+++ src/app/modules/application/components/goodspricebatch/goodspricebatch-query.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'goodspricebatch-query',
+  templateUrl: './goodspricebatch-query.html'
+})
+export class GoodsPriceBatchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
